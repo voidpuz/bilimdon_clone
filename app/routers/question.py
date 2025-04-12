09849081c82849cdd687d2f774e4ae85b6318435
voidpuz <voidpointer07@gmail.com>
2025-04-12 20:37:22 +0500

    add: question update, delete

--- app/routers/question.py
+++ app/routers/question.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 
 from app.dependencies import db_dep, current_user_dep
 from app.models import Question
-from app.schemas import QuestionResponse, QuestionCreate
+from app.schemas import QuestionResponse, QuestionCreate, QuestionUpdate
 
 
 router = APIRouter(prefix="/questions", tags=["questions"])
@@ -42,3 +42,51 @@ async def create_question(
     db.refresh(db_question)
 
     return db_question
+
+
+@router.put("/update/{id}", response_model=QuestionResponse)
+async def update_question(
+        id: int, 
+        question: QuestionUpdate, 
+        db: db_dep
+    ):
+    db_question = db.query(Question).filter(Question.id == id).first()
+
+    if not db_question:
+        raise HTTPException(
+            status_code=404,
+            detail="Question not found."
+        )
+
+    db_question.title = question.title if question.title else db_question.title
+    db_question.description = question.description if question.description else db_question.description
+    db_question.topic_id = question.topic_id if question.topic_id else db_question.topic_id
+
+    db.commit()
+    db.refresh(db_question)
+
+    return db_question
+
+
+@router.delete("/delete/{id}")
+async def delete_question(id: int, db: db_dep):
+    db_question = db.query(Question).filter(Question.id == id).first()
+
+    if not db_question:
+        raise HTTPException(
+            status_code=404,
+            detail="Question not found."
+        )
+
+    db.delete(db_question)
+    db.commit()
+
+    return {
+        "question_id": id,
+        "message": "Question deleted."
+    }
+
+
+# @router.get("{id}/options/", response_model=QuestionWithOptionsResponse)
+# async def get_question_with_options(id: int, db: db_dep):
+#     pass
--- app/schemas/question.py
+++ app/schemas/question.py
@@ -32,3 +32,9 @@ class QuestionCreate(BaseModel):
     title: str
     description: str | None = None
     topic_id: int
+
+
+class QuestionUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    topic_id: int | None = None
