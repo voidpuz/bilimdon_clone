b92fec415384dc771249453d0472243b37266d1d
jamoliddin007 <jamoliddin_007>
2025-04-19 16:57:53 +0500

    main is eddited

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.openapi.models import APIKey, APIKeyIn, SecuritySchemeType
+from fastapi.openapi.utils import get_openapi
+from fastapi.security import HTTPBearer
+
 from typing import Union
 import time
 from datetime import datetime
-
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
 
 from app.routers.auth import router as auth_router
 from app.routers.question import router as question_router
 from app.routers.option import router as option_router
 from app.routers.topic import router as topic_router
 from app.routers.game import router as game_router
-from app.routers.user import router as user_router
-from app.dependencies import get_admin_user, current_user_dep
+from app.routers.participation import router as p_router
+from app.routers.submission import router as sub_router
 
 
 app = FastAPI()
@@ -27,4 +30,34 @@ app.include_router(question_router)
 app.include_router(option_router)
 app.include_router(topic_router)
 app.include_router(game_router)
-app.include_router(user_router)
+app.include_router(p_router)
+app.include_router(sub_router)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="Bilimdon Clone API",
+        version="0.0.1",
+        description="API with JWT-based Authentication",
+        routes=app.routes,
+    )
+
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+
+    for path in openapi_schema["paths"].values():
+        for method in path.values():
+            method.setdefault("security", []).append({"BearerAuth": []})
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
