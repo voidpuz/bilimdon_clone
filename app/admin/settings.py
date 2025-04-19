da22fbc3f735eefb9163a0e249b63c6e195fb965
Samandar-Komilov <komilov.samandar07@gmail.com>
2025-04-19 20:13:01 +0500

    fix: lesson 1 process

--- app/admin/settings.py
+++ app/admin/settings.py
@@ -1,8 +1,8 @@
 from starlette_admin.contrib.sqla import Admin
 
 from app.database import engine
-from app.models import User
-from app.admin.views import UserView
+from app.models import User, Game, Question, Participation, Submission, Option, Topic
+from app.admin.views import UserView, GameView, QuestionView, ParticipationView, SubmissionView, OptionView, TopicView
 
 
 admin = Admin(
@@ -12,3 +12,9 @@ admin = Admin(
 
 
 admin.add_view(UserView(User, icon="fa fa-user"))
+admin.add_view(GameView(Game, icon="fa fa-trophy"))
+admin.add_view(QuestionView(Question, icon="fa fa-question"))
+admin.add_view(ParticipationView(Participation, icon="fa fa-users"))
+admin.add_view(SubmissionView(Submission, icon="fa fa-car"))
+admin.add_view(OptionView(Option, icon="fa fa-notes"))
+admin.add_view(TopicView(Topic, icon="fa fa-topic"))
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,8 +1,29 @@
 from starlette_admin.contrib.sqla import ModelView
 
-from app.models import User
-from app.admin.settings import admin
-
 
 class UserView(ModelView):
     fields = ["id", "email", "first_name", "last_name", "is_active", "is_staff", "is_superuser"]
+
+
+class GameView(ModelView):
+    fields = ["id", "title", "description", "start_time", "end_time", "topic_id", "score"]
+
+
+class QuestionView(ModelView):
+    fields = ["id", "title", "description", "topic_id"]
+
+
+class ParticipationView(ModelView):
+    fields = ["id", "user_id", "game_id", "start_time", "end_time", "gained_score"]
+
+
+class SubmissionView(ModelView):
+    fields = ["id", "user_id", "game_id", "question_id", "option_id", "is_correct"]
+
+
+class OptionView(ModelView):
+    fields = ["id", "question_id", "title", "is_correct"]
+
+
+class TopicView(ModelView):
+    fields = ["id", "name"]
